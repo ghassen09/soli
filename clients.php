4596820bf32381b89cbddcf6fcbc93d40b2d9b96
houssemhannachi <88657175+Ahmedkh10@users.noreply.github.com>
2021-08-09 14:01:10 +0100

    Update clients.php
    @ghassen09

--- clients.php
+++ clients.php
@@ -61,7 +61,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 					<div class="col-sm-6 col-md-3">  
 						<div class="form-group form-focus">
 							<input type="text" class="form-control floating">
-							<label class="focus-label">ghassen client name</label>
+							<label class="focus-label">Client name</label>
 						</div>
 					</div>
 					<div class="col-sm-6 col-md-3">  
