2982cdb38ebe49098beb136ea46add7d25cb6095
houssemhannachi <88657175+Ahmedkh10@users.noreply.github.com>
2021-08-09 13:04:00 +0100

    Update clients.php
    @ghassen09

--- clients.php
+++ clients.php
@@ -90,7 +90,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 										<th>Téléphone</th>
 										<th>Pays</th>
 										<th>Matricule</th>
-										<th>action</th>
+										<th>Action</th>
 									</tr>
 								</thead>
 								<?php 
